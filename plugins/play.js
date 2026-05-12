f3634960ed2150dc24f0fca78048da615a223dd3
baggashermd1-crypto <baggashermd1@gmail.com>
2026-05-12 12:25:07 +0500

    Update play.js

--- plugins/play.js
+++ plugins/play.js
@@ -108,7 +108,7 @@ cmd({
 *│❀ 📁 𝐅𝐨𝐫𝐦𝐚𝐭:* mp3
 *│❀ ⚙️ 𝐒𝐭𝐚𝐭𝐮𝐬:* Downloading...
 *╰┄─̣┄─̇─̣┄─̇─̣┄─̇─̣┄─̇─̣─̇─̣─᛭*
-> ᴘᴏᴡᴇʀᴇᴅ ʙʏ LOVE-ᎷᎠ`
+> ᴘᴏᴡᴇʀᴇᴅ ʙʏ BAGGA-SHER-MD`
 
         await conn.sendMessage(from, {
             image: { url: vid.thumbnail },

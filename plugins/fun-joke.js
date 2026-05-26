e8f7bd37b8f52792374897627c40aabc7461fb8e
baggashermd1-crypto <baggashermd1@gmail.com>
2026-05-26 08:25:31 +0500

    Update joke source attribution in fun-joke.js

--- plugins/fun-joke.js
+++ plugins/fun-joke.js
@@ -17,7 +17,7 @@ cmd({
       return reply("❌ Failed to fetch a joke. Please try again.");
     }
 
-    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *© Powered by JawadTechX*`;
+    const jokeMessage = `🤣 *Here's a random joke for you!* 🤣\n\n*${joke.setup}*\n\n${joke.punchline} 😆\n\n> *© Powered by BAGGA-SHERechX*`;
 
     return reply(jokeMessage);
   } catch (error) {
